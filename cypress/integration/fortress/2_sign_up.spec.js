3f215b7f492ca202981a235d8e73dcbbdc249116
alexzavg <me41zavgorodnii@gmail.com>
2021-04-26 19:19:37 +0300

    implemented MAILSLURP for testing emails

--- cypress/integration/fortress/2_sign_up.spec.js
+++ cypress/integration/fortress/2_sign_up.spec.js
@@ -12,11 +12,8 @@ describe('Sign Up New Customer', function() {
     const dashboardLink = Cypress.env('urls').dashboard;
 
     const firstName = 'test' + getRandomCharLength(8);
-    const lastName = firstName;
-    const email = 'zavgorodniialexander16+' + getRandomCharLength(10) + '@gmail.com';
     const phoneNumber = '+38067' + getRandomNumberLength(7);
     const personalUrl = 'test' + getRandomCharLength(15);
-    const companyName = personalUrl;
     const taxNumber = getRandomNumberLength(6);
     const numberOfEmployees = getRandomNumberLength(1);
     const companyWebAddress = 'https://' + getRandomCharLength(20) + '.com';
@@ -26,37 +23,49 @@ describe('Sign Up New Customer', function() {
     const zip = getRandomNumberLength(6);
     const password = getRandomCharLength(4) + getRandomNumberLength(4);
 
-    const mailSubject = 'Fortress - Email verification';
-    const mailBody = 'Please verify your email by providing the following code';
-
-    let formattedKey, formattedToken;
+    let inboxId, email, confirmationCode, formattedKey, formattedToken;
 
     it('should sign up as new customer', function() {
         
         cy.visit(signUpLink);
 
-        cy.get(signUpPage.firstNameField).type(firstName);
-        cy.get(signUpPage.lastNameField).type(lastName);
-        cy.get(signUpPage.emailField).type(email);
-        cy.get(signUpPage.phoneNumberField).type(phoneNumber);
-        cy.get(signUpPage.personalUrlField).type(personalUrl);
-        cy.get(signUpPage.btnContinueStep1).click();
+        cy.createInbox().then((inbox) => {
+            assert.isDefined(inbox);
+            inboxId = inbox.id;
+            email = inbox.emailAddress;
 
-        cy.get(signUpPage.companyNameField).type(companyName);
-        cy.get(signUpPage.taxNumberField).type(taxNumber);
-        cy.get(signUpPage.numberOfEmployeesField).type(numberOfEmployees);
-        cy.get(signUpPage.companyWebAddressField).type(companyWebAddress);
-        cy.get(signUpPage.btnContinueStep2).click();
-
-        cy.get(signUpPage.countryDropdown).select(country).should('have.value', 'UA');
-        cy.get(signUpPage.stateDropdown).select(state).should('have.value', '53');
-        cy.get(signUpPage.cityField).type(city);
-        cy.get(signUpPage.zipField).type(zip);
-        cy.get(signUpPage.btnContinueStep3).click();
-
-        cy.get(signUpPage.passwordField).type(password);
-        cy.get(signUpPage.confirmPasswordField).type(password);
-        cy.get(signUpPage.btnContinueStep4);
+            cy.get(signUpPage.firstNameField).type(firstName);
+            cy.get(signUpPage.lastNameField).type(firstName);
+            cy.get(signUpPage.emailField).type(email);
+            cy.get(signUpPage.phoneNumberField).type(phoneNumber);
+            cy.get(signUpPage.personalUrlField).type(personalUrl);
+            cy.get(signUpPage.btnContinueStep1).click();
+    
+            cy.get(signUpPage.companyNameField).type(personalUrl);
+            cy.get(signUpPage.taxNumberField).type(taxNumber);
+            cy.get(signUpPage.numberOfEmployeesField).type(numberOfEmployees);
+            cy.get(signUpPage.companyWebAddressField).type(companyWebAddress);
+            cy.get(signUpPage.btnContinueStep2).click();
+    
+            cy.get(signUpPage.countryDropdown).select(country).should('have.value', 'UA');
+            cy.get(signUpPage.stateDropdown).select(state).should('have.value', '53');
+            cy.get(signUpPage.cityField).type(city);
+            cy.get(signUpPage.zipField).type(zip);
+            cy.get(signUpPage.btnContinueStep3).click();
+    
+            cy.get(signUpPage.passwordField).type(password);
+            cy.get(signUpPage.confirmPasswordField).type(password);
+            cy.get(signUpPage.btnContinueStep4).click();
+    
+            cy.waitForLatestEmail(inboxId).then((email) => {
+                assert.isDefined(email);
+                assert.strictEqual(/code:/.test(email.body), true);
+                let body = email.body;
+                confirmationCode = body.split('code: ')[1].slice(0,6); // get confirmation code from email
+                cy.log(confirmationCode);
+            });
+            
+        });
 
     });
 
