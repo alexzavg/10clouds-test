af5fe0a4d6765d2612e2fad523f982a3ba617968
alexzavg <me41zavgorodnii@gmail.com>
2021-04-26 10:23:00 +0300

    implementing sign up scenario

--- cypress/integration/fortress/2_sign_up.spec.js
+++ cypress/integration/fortress/2_sign_up.spec.js
@@ -4,6 +4,7 @@ import {requests} from '../../support/requests.js';
 import {getRandomCharLength, getRandomNumberLength} from '../../support/dataGenerator.js';
 
 const {generateToken} = require('authenticator');
+let imaps = require('imap-simple');
 
 describe('Sign Up New Customer', function() {
 
@@ -12,7 +13,42 @@ describe('Sign Up New Customer', function() {
     const dashboardLink = Cypress.env('urls').dashboard;
 
     const firstName = 'test' + getRandomCharLength(8);
+    const lastName = firstName;
+    const email = 'zavgorodniialexander16+' + getRandomCharLength(10) + '@gmail.com';
+    const phoneNumber = '+38067' + getRandomNumberLength(7);
+    const personalUrl = 'test' + getRandomCharLength(15);
+    const companyName = personalUrl;
+    const taxNumber = getRandomNumberLength(6);
+    const numberOfEmployees = getRandomNumberLength(1);
+    const companyWebAddress = 'https://' + getRandomCharLength(20) + '.com';
+    const country = 'Ukraine';
+    const state = 'Poltavs\'ka Oblast\'';
+    const city = 'Poltava';
+    const zip = getRandomNumberLength(6);
+    const password = getRandomCharLength(4) + getRandomNumberLength(4);
 
+    const mailSubject = 'Fortress - Email verification';
+    const mailBody = 'Please verify your email by providing the following code';
+
+    let fetchOptions = {
+        bodies: ['TEXT'],
+        markSeen: false
+    };
+
+    let config = {
+        imap: {
+            user: 'zavgorodniialexander16@gmail.com',
+            password: 'WA12aszx34',
+            host: 'imap.gmail.com',
+            port: 993,
+            tls: true,
+            tlsOptions: { "servername": "imap.gmail.com" },
+            markSeen: true,
+            authTimeout: 120000
+        }
+    };
+
+    let messageSince, confirmationCode;
     let formattedKey, formattedToken;
 
     it('should sign up as new customer', function() {
--- cypress/pages/sign-up.js
+++ cypress/pages/sign-up.js
@@ -1,3 +1,18 @@
 export const signUpPage = {
-        'firstNameField': '[formcontrolname="firstName"]'
+    'firstNameField': '[formcontrolname="firstName"]',
+    'lastNameField': '[formcontrolname="lastName"]',
+    'emailField': '[formcontrolname="email"]',
+    'phoneNumberField': '[formcontrolname="phoneNumber"]',
+    'personalUrlField': '[formcontrolname="siteUrl"]',
+    'companyNameField': '[formcontrolname="companyName"]',
+    'taxNumberField': '[formcontrolname="taxNumber"]',
+    'numberOfEmployeesField': '[formcontrolname="numberOfUsers"]',
+    'companyWebAddressField': '[formcontrolname="companyWebAddress"]',
+    'countryDropdown': '[formcontrolname="country"]',
+    'stateDropdown': '[formcontrolname="state"]',
+    'cityField': '[formcontrolname="city"]',
+    'zipField': '[formcontrolname="zip"]',
+    'passwordField': '[formcontrolname="password"]',
+    'confirmPasswordField': '[formcontrolname="confirmPassword"]',
+    'emailCodeField': '[formcontrolname="code"]'
 }
