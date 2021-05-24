28468999837dc414f50c6bb5a51b36b190e26c7b
alexzavg <me41zavgorodnii@gmail.com>
2021-05-24 17:09:58 +0300

    added mssp invite

--- cypress/integration/fortress/5_mssp.spec.js
+++ cypress/integration/fortress/5_mssp.spec.js
@@ -14,68 +14,139 @@ describe('MSSP', function() {
     const msspLink = Cypress.env('urls').mssp;
     const signUpLink = Cypress.env('urls').signUp; 
     const serverId = Cypress.env('MAILOSAUR_SERVER_ID');
-    const userName = 'Autotest Autotest';
-    const email = Cypress.env('users').fourth.email;
-    const password = Cypress.env('users').fourth.password;
-    const formattedKey = Cypress.env('users').fourth.formattedKey;
+    const userNameFirst = 'Autotest Autotest';
+    const emailFirst = Cypress.env('users').fourth.email;
+    const passwordFirst = Cypress.env('users').fourth.password;
+    const formattedKeyFirst = Cypress.env('users').fourth.formattedKey;
+    const userNameSecond = 'Autotests Autotests';
+    const emailSecond = Cypress.env('users').fifth.email;
+    const passwordSecond = Cypress.env('users').fifth.password;
+    const formattedKeySecond = Cypress.env('users').fifth.formattedKey;
     const currentTime = getCurrentTimeISO();
     const customerEmailFirst = getRandomCharLength(15) + getRandomNumberLength(5) + '@' + serverId + '.mailosaur.net';
+    const customerEmailSecond = getRandomCharLength(15) + getRandomNumberLength(5) + '@' + serverId + '.mailosaur.net';
     
-    let formattedToken;
+    describe('Invite [Regular] company', function() {
 
-    it('should login', function() {
-        cy.visit(signInLink);
-        cy.url().should('eq', signInLink);
+        it('should login', function() {
+            cy.visit(signInLink);
+            cy.url().should('eq', signInLink);
+    
+            let formattedToken = generateToken(formattedKeyFirst);
+            cy.log('Google OTP is:', formattedToken);
+            let array = Array.from(formattedToken);
+            cy.log(array);
+    
+            cy.signIn(emailFirst, passwordFirst);
+            cy.fillOtp(array[0], array[1], array[2], array[3], array[4], array[5]);
+        });
+    
+        it('should invite [REGULAR] company & check invitation link from mail', function() {
+            cy.intercept(requests['customer-search']).as('customer-search');
+            cy.intercept(requests['customer-invitations']).as('customer-invitations');
+            
+            cy.get(signUpPageElements.spinner).should('not.exist').then(() => {
+                cy.get(dashboardPageElements.scoreValue).should('be.visible');
+    
+                // ! menu categories text isn't displayed when navbar is expanded, workaround:
+                cy.get(navbarElements.fortressLogoTop).click();
+                cy.wait(2000);
+                cy.contains(navbarElements.category, navbarData.settings).click();
+                cy.contains(navbarElements.category, navbarData.msspConfiguration).click();
+            });
+    
+            cy.url().should('eq', msspLink);
+            cy.get(signUpPageElements.spinner).should('not.exist').then(() => {
+                cy.wait('@customer-search').its('response.statusCode').should('eq', 200);
+    
+                cy.get(msspPageElements.inviteCompanyBtn).click();
+                cy.get(msspPageElements.customerTypeDropdown).select(msspPageData.customerType.regular);
+                cy.get(msspPageElements.emailField).type(customerEmailFirst);
+                cy.get(msspPageElements.inviteBtn).click();
+    
+                cy.wait('@customer-invitations').its('response.statusCode').should('eq', 201);
+            });
+    
+            cy.mailosaurGetMessage(serverId, {
+                sentFrom: emailsData.emails.support,
+                sentTo: customerEmailFirst,
+                subject: emailsData.subjects.customerInvitation + userNameFirst
+            }, {
+                receivedAfter: new Date(currentTime),
+                timeout: 60000
+            }).then(mail => {
+                const invitationLink = mail.html.links[0].href;
+                cy.log('Invitation link:', invitationLink);
+    
+                cy.visit(invitationLink);
+                cy.url().should('contain', signUpLink);
+                cy.get(signUpPageElements.emailField).should('have.value', customerEmailFirst);
+            });
 
-        formattedToken = generateToken(formattedKey);
-        cy.log('Google OTP is:', formattedToken);
-        let array = Array.from(formattedToken);
-        cy.log(array);
-
-        cy.signIn(email, password);
-        cy.fillOtp(array[0], array[1], array[2], array[3], array[4], array[5]);
+            cy.clearCookies();
+            cy.clearLocalStorage();
+        });
     });
 
-    it('should invite [REGULAR] company & check invitation link from mail', function() {
-        cy.intercept(requests['customer-search']).as('customer-search');
-        cy.intercept(requests['customer-invitations']).as('customer-invitations');
-        
-        cy.get(signUpPageElements.spinner).should('not.exist').then(() => {
-            cy.get(dashboardPageElements.scoreValue).should('be.visible');
+    describe('Invite [MSSP] company', function() {
 
-            // ! menu categories text isn't displayed when navbar is expanded, workaround:
-            cy.get(navbarElements.fortressLogoTop).click();
-            cy.wait(2000);
-            cy.contains(navbarElements.category, navbarData.settings).click();
-            cy.contains(navbarElements.category, navbarData.msspConfiguration).click();
+        it('should login', function() {
+            cy.visit(signInLink);
+            cy.url().should('eq', signInLink);
+    
+            let formattedToken = generateToken(formattedKeySecond);
+            cy.log('Google OTP is:', formattedToken);
+            let array = Array.from(formattedToken);
+            cy.log(array);
+    
+            cy.signIn(emailSecond, passwordSecond);
+            cy.fillOtp(array[0], array[1], array[2], array[3], array[4], array[5]);
         });
+    
+        it('should invite [MSSP] company & check invitation link from mail', function() {
+            cy.intercept(requests['customer-search']).as('customer-search');
+            cy.intercept(requests['customer-invitations']).as('customer-invitations');
+            
+            cy.get(signUpPageElements.spinner).should('not.exist').then(() => {
+                cy.get(dashboardPageElements.scoreValue).should('be.visible');
+    
+                // ! menu categories text isn't displayed when navbar is expanded, workaround:
+                cy.get(navbarElements.fortressLogoTop).click();
+                cy.wait(2000);
+                cy.contains(navbarElements.category, navbarData.settings).click();
+                cy.contains(navbarElements.category, navbarData.msspConfiguration).click();
+            });
+    
+            cy.url().should('eq', msspLink);
+            cy.get(signUpPageElements.spinner).should('not.exist').then(() => {
+                cy.wait('@customer-search').its('response.statusCode').should('eq', 200);
+    
+                cy.get(msspPageElements.inviteCompanyBtn).click();
+                cy.get(msspPageElements.customerTypeDropdown).select(msspPageData.customerType.mssp);
+                cy.get(msspPageElements.emailField).type(customerEmailSecond);
+                cy.get(msspPageElements.inviteBtn).click();
+    
+                cy.wait('@customer-invitations').its('response.statusCode').should('eq', 201);
+            });
+    
+            cy.mailosaurGetMessage(serverId, {
+                sentFrom: emailsData.emails.support,
+                sentTo: customerEmailSecond,
+                subject: emailsData.subjects.customerInvitation + userNameSecond
+            }, {
+                receivedAfter: new Date(currentTime),
+                timeout: 60000
+            }).then(mail => {
+                const invitationLink = mail.html.links[0].href;
+                cy.log('Invitation link:', invitationLink);
+    
+                cy.visit(invitationLink);
+                cy.url().should('contain', signUpLink);
+                cy.get(signUpPageElements.emailField).should('have.value', customerEmailSecond);
+            });
 
-        cy.url().should('eq', msspLink);
-        cy.get(signUpPageElements.spinner).should('not.exist').then(() => {
-            cy.wait('@customer-search').its('response.statusCode').should('eq', 200);
-
-            cy.get(msspPageElements.inviteCompanyBtn).click();
-            cy.get(msspPageElements.customerTypeDropdown).select(msspPageData.customerType.regular);
-            cy.get(msspPageElements.emailField).type(customerEmailFirst);
-            cy.get(msspPageElements.inviteBtn).click();
-
-            cy.wait('@customer-invitations').its('response.statusCode').should('eq', 201);
-        });
-
-        cy.mailosaurGetMessage(serverId, {
-            sentFrom: emailsData.emails.support,
-            sentTo: customerEmailFirst,
-            subject: emailsData.subjects.customerInvitation + userName
-        }, {
-            receivedAfter: new Date(currentTime),
-            timeout: 60000
-        }).then(mail => {
-            const invitationLink = mail.html.links[0].href;
-            cy.log('Invitation link:', invitationLink);
-
-            cy.visit(invitationLink);
-            cy.url().should('contain', signUpLink);
-            cy.get(signUpPageElements.emailField).should('have.value', customerEmailFirst);
+            cy.clearCookies();
+            cy.clearLocalStorage();
         });
     });
  
