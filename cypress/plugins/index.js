8af36ccc39fa151886b027f7300836cfe569f52a
alexzavg <me41zavgorodnii@gmail.com>
2021-05-30 19:36:47 +0300

    viewport fix

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,7 +18,7 @@
 module.exports = (on, config) => {
   on('before:browser:launch', (browser = {}, launchOptions) => {
     if (browser.family === 'chromium' && browser.name !== 'electron') {
-      launchOptions.args.push('--window-size=320,240');
+      launchOptions.args.push('--window-size=1920,1080');
 
       // auto open devtools
       // launchOptions.args.push('--auto-open-devtools-for-tabs')
