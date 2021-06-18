fdb698d5ee390e233c0bd02faf85e93259671880
Multiband EQ <me41zavgorodnii@gmail.com>
2021-06-18 15:51:53 +0300

    done first test

--- cypress/integration/api/api.spec.js
+++ cypress/integration/api/api.spec.js
@@ -1,6 +1,6 @@
 import {dashboardPageElements} from '../../components/dashboard.js';
 import {signUpPageElements} from '../../components/sign-up.js';
-import {requests} from '../../support/requests.js';
+import {endpoints} from '../../support/endpoints.js';
 
 const {generateToken} = require('authenticator');
 
@@ -13,28 +13,53 @@ describe('API', function() {
     const formattedKey = Cypress.env('apiSuite').user.formattedKey;
 
     let formattedToken;
- 
-    it('Sign in', function() {
+
+    before(() => {
+        // sign in
         cy.visit(signInLink);
-        cy.url().should('eq', signInLink);
-
         formattedToken = generateToken(formattedKey);
-        cy.log('Google OTP is:', formattedToken);
         let array = Array.from(formattedToken);
-        cy.log(array);
-
         cy.signIn(email, password);
         cy.fillOtp(array[0], array[1], array[2], array[3], array[4], array[5]);
         cy.get(signUpPageElements.spinner).should('not.exist');
         cy.get(dashboardPageElements.scoreValue).should('be.visible');
+
+        // get data from local storage
+        cy.getLocalStorage('auth.refreshToken').then(val => {
+            let refreshToken = val.replaceAll('"', '');
+            cy.wrap(refreshToken).as('refreshToken');
+        });
+        cy.getLocalStorage('auth.idToken').then(val => {
+            let idToken = val.replaceAll('"', '');
+            cy.wrap(idToken).as('idToken');
+        });
+        cy.getLocalStorage('auth.accessToken').then(val => {
+            let accessToken = val.replaceAll('"', '');
+            cy.wrap(accessToken).as('accessToken');
+        });
+        cy.saveLocalStorage();
     });
 
-    it(`Get [authorization] & [x-id-token]`, function() {
-            
+    beforeEach(() => {
+        cy.restoreLocalStorage();
     });
 
-    it(`Checking endpoint ${baseUrl}/auth/refreshTokens`, function() {
-            
+    it(`Check endpoint ${baseUrl}${endpoints['auth-refresh-tokens']}`, function() {
+        cy.request(
+            {
+                method: 'POST',
+                url: baseUrl + endpoints['auth-refresh-tokens'],
+                body: {
+                    "refreshToken": this.refreshToken,
+                    "idToken": this.idToken
+                }
+            }
+        ).should((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.jwtToken).to.have.lengthOf.greaterThan(0);
+            expect(response.body.refreshToken).to.have.lengthOf.greaterThan(0);
+            expect(response.body.idToken).to.have.lengthOf.greaterThan(0);
+          })
     });
 
 });
---
+++ cypress/support/endpoints.js
@@ -0,0 +1,3 @@
+export const endpoints = {
+  'auth-refresh-tokens':'/auth/refresh-tokens'
+}
