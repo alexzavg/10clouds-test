1bd743082a04917f2338cade1b7d4e7633fd659e
Multiband EQ <me41zavgorodnii@gmail.com>
2021-06-23 14:37:47 +0300

    fixed create new user spec

--- cypress/integration/e2e/users/users.spec.js
+++ cypress/integration/e2e/users/users.spec.js
@@ -19,6 +19,7 @@ describe('Users', function() {
     const serverId              = Cypress.env('MAILOSAUR_SERVER_ID');
     const emailDomain           = Cypress.env('email_domain');
     const newUserFirstName      = 'cypress' + getRandomCharLength(8);
+    const newUserLastName      = 'cypress' + getRandomCharLength(8);
     const newUserEmail          = getRandomCharLength(15) + getRandomNumberLength(5) + '@' + serverId + emailDomain;
     const newUserPhoneNumber    = '+38093' + getRandomNumberLength(7);
     const role                  = 'Organization Admin';
@@ -62,7 +63,7 @@ describe('Users', function() {
     
                 cy.get(usersPageElements.btnAddUser).click();
                 cy.get(usersPageElements.firstNameField).type(newUserFirstName);
-                cy.get(usersPageElements.lastNameField).type(newUserFirstName);
+                cy.get(usersPageElements.lastNameField).type(newUserLastName);
                 cy.get(usersPageElements.emailfield).type(newUserEmail);
                 cy.get(usersPageElements.phoneField).type(newUserPhoneNumber);
                 cy.get(usersPageElements.roleDropdown).select(role);
@@ -72,7 +73,6 @@ describe('Users', function() {
             cy.clearCookies();
             cy.clearLocalStorage();
         
-            // get verification code from email
             cy.mailosaurGetMessage(serverId, {
                 sentFrom: emailsData.emails.support,
                 sentTo: newUserEmail,
@@ -81,10 +81,12 @@ describe('Users', function() {
                 receivedAfter: new Date(currentTime),
                 timeout: 60000
             }).then(mail => {
-                const body = mail.html.body;
-                temporaryPassword = body.split('verification code: <b>')[1].slice(0,8);
+                const body = mail.text.body;
+                expect(body).to.contain(newUserFirstName);
+                expect(body).to.contain(newUserLastName);
+                temporaryPassword = body.split('verification code: *')[1].slice(0,8);
                 cy.log('Temporary password is', temporaryPassword);
-    
+
                 cy.visit(signInLink);
                 cy.url().should('eq', signInLink);
     
@@ -106,7 +108,7 @@ describe('Users', function() {
     
                     cy.clearCookies();
                     cy.clearLocalStorage();
-                });    
+                });
             });
         });
 
@@ -134,7 +136,7 @@ describe('Users', function() {
                     cy.wait('@device-search').its('response.statusCode').should('eq', 200);
                 });
     
-                cy.get(usersPageElements.searchField).type(newUserFirstName+'{enter}').then(() => {
+                cy.get(usersPageElements.searchField).type(newUserEmail+'{enter}').then(() => {
                     cy.get(signUpPageElements.spinner).should('not.exist').then(() => {
                         cy.contains('tr', newUserEmail).parent().within(() => {
                             cy.get(usersPageElements.kebabMenu).click();
