4eb8701e3d830afb962dac3ae76d72fa0a671e1d
alexzavg <me41zavgorodnii@gmail.com>
2021-07-01 08:05:09 +0300

    comment

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -47,6 +47,7 @@ module.exports = (on, config) => {
     }
   })
   on('after:spec', (spec, results) => {
+    // delete video if test passes
     if (results && results.stats.failures === 0 && results.video) {
       // `del()` returns a promise, so it's important to return it to ensure
       // deleting the video is finished before moving on
